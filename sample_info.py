f24dddedce27a216f22917c4bc5930fca2282535
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-10-02 13:20:06 +0530

    Add files via upload

---
+++ sample_info.py
@@ -0,0 +1,24 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 18293272
+API_HASH = '46b802cadbce8893e42fbe40dbe4d08f'
+BOT_TOKEN = '5706689401:AAErpu2QK1d-7nVBFCr6KAijA6cez8JXBQ8'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [1849214067, 'admin123',]
+CHANNELS = [1001709742551, 'channelusername']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Moviesindna/moviefinderbot.git /  moviefinderbot        
+
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /moviefinderbot
+fi
+cd /moviefinderbot
+pip3 install -U -r requirements.txt
+echo "Starting Bot....💥"
+python3 bot.py
