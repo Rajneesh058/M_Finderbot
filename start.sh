03aa953c461c3fe93d9c67999039350600342fbd
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-10-02 15:49:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /moviefinderbot
 fi
-cd /moviefinderbot
+cd  moviefinderbot
 pip3 install https://github.com/Rajneesh058/moviefinderbot/blob/76136110a0043b6fe426ec20b4e21448f53ddafe/requirements.txt
 echo "Starting Bot....💥"
 python3 https://github.com/Rajneesh058/moviefinderbot/blob/4e40892df4dd01ccd1f665fd5f8d37d87e7533fb/bot.py
