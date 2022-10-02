64edfbd0a5f21c3b32010dbabadca764b7c4a80e
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-10-02 13:25:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Moviesindna/moviefinderbot.git /  moviefinderbot        
+  git clone https://github.com/Rajneesh058/moviefinderbot.git / moviefinderbot        
 
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /moviefinderbot
